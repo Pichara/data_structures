1e4d2dccb64ff028c1d5c42c7b977f6d779010a6
Rodrigo Pichara <rgomes8106@conestogac.on.ca>
2025-03-07 18:30:07 -0500

    Create binary_search.py
    Added binary_search, in Python... yes. Python does not need it because of the implementation of lists with index() function, however it demonstrates how it works

---
+++ binary_search.py
@@ -0,0 +1,22 @@
+#If I want for example to find the value 7 in ORDERED array, I can use binary search for less memory usage!
+#Of course you can use index() in python, but this is a linear search
+
+#You can do the binary search like this:
+array = [1, 3, 4, 6, 7, 9, 10, 12, 14, 15, 17, 19, 21, 23, 25, 27, 28, 30, 31]
+number = 31
+
+start_of_array = 0
+end_of_array = len(array) - 1
+
+while start_of_array <= end_of_array:
+    mid_index = (start_of_array + end_of_array) // 2
+    
+    if array[mid_index] == number:
+        print(f"Number is in the index: {mid_index}")
+        break
+    elif number < array[mid_index]:
+        end_of_array = mid_index - 1
+    else:
+        start_of_array = mid_index + 1
+else:
+    print("Number not found!")
